48c36c2a72122b386d0a27daed9bcf3643f980f0
Görkem Tabak <sagortab@gmail.com>
2020-05-26 20:53:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,28 +9,48 @@ var filesToCache = [
     '/images',
 ]
 
-self.addEventListener('install', function (event) {
-    console.log('Install');
-    event.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(filesToCache)
-        })
-    )
-})
+self.addEventListener('install', async e => {
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(assets);
+    return self.skipWaiting();
+});
 
-self.addEventListener('activate', function (e) {
-    console.log('Activate');
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== cacheName) {
-                    return caches.delete(key)
-                }
-            }))
-        })
-    )
-})
 
+
+self.addEventListener('activate', e => {
+    self.clients.claim().then(r => {
+        console.log("service worker activated");
+    });
+});
+
+self.addEventListener('fetch', async e => {
+    const req = e.request;
+    const url = new URL(req.url);
+
+    if (url.origin === location.origin) {
+        e.respondWith(cacheFirst(req));
+    } else {
+        e.respondWith(networkAndCache(req));
+    }
+});
+
+async function cacheFirst(req) {
+    const cache = await caches.open(staticCacheName);
+    const cached = await cache.match(req);
+    return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+    const cache = await caches.open(staticCacheName);
+    try {
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
+}
 self.addEventListener('fetch', function (e) {
     console.log(e.request);
     e.respondWith(
