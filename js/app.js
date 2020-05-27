868a1cb38c2ee3df90478a9788b51b951b77a617
Görkem Tabak <sagortab@gmail.com>
2020-05-27 19:15:21 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('sw.js')
-		.then(reg => console.log('service worker is registered', reg))
-		.catch(err => console.log('service worker not registered', err));
+if ('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js').then(function(registration){
+    console.log('service worker registration succeeded:',registration);
+  },
+function(error){
+  console.log('service worker registration failed:',error);
+});
+}
+else{
+  console.log('service workers are not supported.');
 }
 
 navigator.serviceWorker.register('/sw.js').then(reg => {
